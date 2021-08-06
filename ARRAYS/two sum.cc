41d60f96101f1c17651e9263c0f10a3db470fce2
Anand-Raj-CSE <anandrajcode@gmail.com>
2021-08-06 19:53:21 +0530

    Two sum

--- ARRAYS/two sum.cc
+++ ARRAYS/two sum.cc
@@ -1,6 +1,6 @@
 #include<iostream>
 #include<vector>
-#
+#include<unordered_set>
 using namespace std;
 
 // Brute force - O(n2)
@@ -13,8 +13,8 @@ vector<int> twoSumbf(vector<int>& nums, int target) {
             {
                 if(nums[i]+nums[j] == target)
                 {
-                    ans.push_back(i);
-                    ans.push_back(j);
+                    ans.push_back(nums[i]);
+                    ans.push_back(nums[j]);
                 }
             }
         }
@@ -23,19 +23,49 @@ vector<int> twoSumbf(vector<int>& nums, int target) {
 
 // Little Optimized - O(NlogN)
 
-
-
-// Optimized using Hashmap - O(N) - Looking up i.e Searching in a set is O(1) operation
-
 vector<int> twoSumBS(vector<int>& nums, int target) {
         vector<int> ans;        
         int n = nums.size();
         for(int i=0;i<n;i++)
         {
+            int f = target-nums[i];
+            int s=0,e=n-1;
+            while (s<=e)
+            {
+                int mid = s+(e-s)/2;
+                if(nums[mid] == f)
+                {
+                    ans.push_back(nums[i]);
+                    ans.push_back(nums[mid]);
+                }
+                else if(nums[mid]<f)
+                s=mid+1;
+                else
+                e=mid-1;
+            }
+            
+        }
+        return ans;
+    }
+
+// Optimized using Hashmap - O(N) - Looking up i.e Searching in a set is O(1) operation
+
+vector<int> twoSumOPT(vector<int>& nums, int target) {
+        vector<int> ans;        
+        int n = nums.size();
+        unordered_set<int> s;
+        for(int i=0;i<n;i++)
+        {
             // We would not first insert all the elements in the set as we cout get as 2+2=4 , if 4 is target.
             // This would give us wrong answer.So we would keep adding to set while iterating and finding.
-
-
+            int f = target-nums[i];
+            if(s.find(f)!=s.end())
+            {
+                ans.push_back(f);
+                ans.push_back(nums[i]);
+            }
+            // Now if not found then insert
+            s.insert(nums[i]);
         }
         return ans;
     }
