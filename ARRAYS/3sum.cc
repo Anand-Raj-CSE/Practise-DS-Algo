b7e2b84a0f5009f7a624d19e6cf09c8d8e7db336
Anand-Raj-CSE <anandrajcode@gmail.com>
2021-08-07 22:52:45 +0530

    Optimised code to n2logn from n3

--- ARRAYS/3sum.cc
+++ ARRAYS/3sum.cc
@@ -3,7 +3,7 @@
 #include<algorithm>
 using namespace std;
 
-vector<vector<int>> threeSumBF(vector<int>& nums,int tagrget) {
+vector<vector<int>> 3SumBF(vector<int>& nums,int tagrget) {
         vector<vector<int>> ans;
         int n = nums.size();
         for(int i=0;i<n;i++)
@@ -32,14 +32,42 @@ vector<vector<int>> threeSumBF(vector<int>& nums,int tagrget) {
         return ans;
     }
 
-vector<vector<int>> threeSumBF(vector<int>& nums,int tagrget) {
+vector<vector<int>> 3sumOPT(vector<int>& nums,int target) 
+{
         vector<vector<int>> ans;
         int n = nums.size();
         // 1st we would sort the entire array
         sort(nums.begin(),nums.end());
+        // Now pick every number and then solve pair sum problrm for every part
+        // We would use two pointer approach as the array has been sorted
+        for(int i=0;i<=n-3;i++)
+        {
+            int j = i+1;
+            int k = n-1;
+            // Two Pointer approach
+            while(j<k)
+            {
+                int current_sum = nums[i];
+                current_sum+=nums[j];
+                current_sum+=nums[k];
+                if(current_sum == target)
+                {
+                    // Better Way to push elements in 2d vector
+                    ans.push_back({nums[i],nums[j],nums[k]});
+                    j++;
+                    k--;
+                }
+                else if(current_sum > target)
+                {
+                    k--;
+                }
+                else
+                j++;
+            }
+        }
 
         return ans;
-    }
+}
 
 int main()
 {
@@ -54,7 +82,7 @@ int main()
     }
     int ta;cin>>ta;
     // Brute Force
-    vector<int> ans = 3sumBF(a,ta);
+    vector<vector<int>> ans = 3sumBF(a,ta);
     for(auto j:ans)
     {
         for(auto k:j)
@@ -64,7 +92,7 @@ int main()
         cout<<endl;
     }
     // Optimized one
-    vector<int> ans = 3sumOPT(a,ta);
+    vector<vector<int>> ans = 3sumOPT(a,ta);
     for(auto j:ans)
     {
         for(auto k:j )
