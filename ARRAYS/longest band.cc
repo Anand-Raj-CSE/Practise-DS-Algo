35432f8dd1b84161f173e158b86649bb80667cd5
Anand-Raj-CSE <anandrajcode@gmail.com>
2021-08-15 19:12:04 +0530

    Started longest band problem

---
+++ ARRAYS/longest band.cc
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/longest-consecutive-sequence/
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int> a;
+    int t;
+    for (int i = 0; i < n; i++)
+    {
+        cin>>t;
+        a.push_back(t);
+    }
+
+
+
+    return 0;
+}
