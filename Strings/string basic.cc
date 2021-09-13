2bb3e1753320c45dff2336fc157b7069b29b7fc6
Anand-Raj-CSE <anandrajcode@gmail.com>
2021-09-13 19:36:54 +0530

    Added find function to the existing program

--- Strings/string basic.cc
+++ Strings/string basic.cc
@@ -1,6 +1,22 @@
 #include<iostream>
 #include<string>
+#include<vector>
 using namespace std;
+
+// Function to print the indices where small string is found in the big string
+vector<int> stringSearch(string big,string small){
+    //store the occurrences in the result vector
+    vector<int> result = {};
+    int idx = big.find(small);
+    while(idx!= -1)
+    {
+        result.push_back(idx);
+        idx = big.find(small,idx+1);
+    }
+    return result;
+
+}
+
 
 int main()
 {
@@ -17,5 +33,6 @@ int main()
     cout<<"found at : "<<idx;
     else
     cout<<"Not found"<<endl;
+
     return 0;
 }
