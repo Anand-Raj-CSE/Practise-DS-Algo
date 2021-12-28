34f91faacf0b025473a44ab67e4a92c8ea548827
Anand-Raj-CSE <anandrajcode@gmail.com>
2021-12-28 19:51:50 +0530

    Learnt about ifndef and ifdef

---
+++ ifndef.cc
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define aanand
+
+int main()
+{
+    int n;cin>>n;
+    #ifndef aanand // if n def means that I have not deifined the macro aanand.
+    cout<<n+1<<endl;
+    #endif // This is the end of ifdef
+    return 0;
+}
